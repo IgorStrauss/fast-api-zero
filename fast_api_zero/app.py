985eabd90cef83a8facec93eb84c00cc2d836862
Igor Marques <engenheirostrauss2@gmail.com>
2024-06-19 20:27:46 -0300

    Adicionando nova rota /path-html para renderizar HTML na página usando HTMLResponse

--- fast_api_zero/app.py
+++ fast_api_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,22 @@ app = FastAPI()
 @app.get("/")
 def read_root() -> dict:
     return {"Message": "Olá Mundo!"}
+
+@app.get("/path-html")
+def read_html() -> HTMLResponse:
+    html_content = """
+    <html>
+        <head>
+            <title>MarquesIgor!</title>
+        </head>
+        <body style="background-color:#1c1c1c;">
+            <h1 style="color:red; text-align:center; margin-top:20rem">
+            MarquesIgor
+            </h1>
+            <h1 style="color:red; text-align:center; margin-top:5rem">
+            Curso FastAPI Zero com Dunossauro!
+            </h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
